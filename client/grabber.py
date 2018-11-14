5775f8624d8d896ca226703f9071485ddba39bc2
Geoffrey Kanteles <geoffreykanteles@gmail.com>
2018-11-13 19:00:05 -0500

    Added basic logging

--- client/grabber.py
+++ client/grabber.py
@@ -1,18 +1,24 @@
+from pathlib import Path
 import time
 import datetime
 import requests
-
+ 
 # get flag status (WAIT/RUN)
 # response = requests.get('http://api.zealot/<endpoint_for_flag>/')
 response = "WAIT"
 
 while response != "RUN":
-    print ("Flag is set to '%s' at: %s" % (response, datetime.datetime.now()))
-    time.sleep(5)
-    # response = requests.get('http://api.zealot/<endpoint_for_flag>/')
+    print ("Flag is set to '%s'" % (response)) # debug print
+    # log flag state
+    # print ("Flag is set to '%s' at: %s" % (response, datetime.datetime.now()), file=open("zealotlog.txt", "a"))
+
+    time.sleep(5)   # wait x seconds
+    # response = requests.get('http://api.zealot/<endpoint_for_flag>/') # query flag, update value
     response = "RUN"
 
-print ("Flag set to '%s' at: %s" % (response, datetime.datetime.now()))
+print ("Flag is set to '%s'" % (response)) # debug print
+# log flag state
+# print ("Flag set to '%s' at: %s" % (response, datetime.datetime.now()), file=open("zealotlog.txt", "a"))
+
 # find all files changed in last x minutes
 # grab files
-
