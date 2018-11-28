c4cc94c0cbad775266665c4886fa0424f52def7a
Chris Partridge <chris@partridge.tech>
2018-11-27 23:41:06 -0500

    uploads working

--- server/web/store/index.php
+++ server/web/store/index.php
@@ -1,10 +1,9 @@
 <?php
 ini_set('display_errors', 'on');
-$pathInfo = pathinfo($_FILES['userFile']['name']);
-$fileName = time() . ".zip"; 
 
+$fileName = time() . ".zip"; 
 $target = "/var/zealot/" . $fileName;
-move_uploaded_file($_FILES['userFile']['tmp_name'], $target);
+move_uploaded_file($_FILES['file']['tmp_name'], $target);
 
 echo "UPLOAD_OK";
 ?>
