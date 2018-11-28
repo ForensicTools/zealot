7b4d7c604417fa58ae710c139e8bb99f855062d9
Chris Partridge <chris@partridge.tech>
2018-11-28 02:06:33 -0500

    Geo-profiling online

--- server/web/collect/index.php
+++ server/web/collect/index.php
@@ -7,6 +7,8 @@ if($mysqli->connect_error) {
     exit;
 }
 
+$showtime = false; // NOW YOU'VE DONE IT https://www.youtube.com/watch?v=kX1ytyCG09g
+
 $dbBlacklist = $mysqli->query("SELECT * FROM `blacklist`");
 $blacklist = [];
 while($getBlacklist = mysqli_fetch_assoc($dbBlacklist)) {
@@ -15,27 +17,77 @@ while($getBlacklist = mysqli_fetch_assoc($dbBlacklist)) {
 
 $dbLive = $mysqli->query("SELECT * FROM `live_stats`");
 $liveData = [];
-$showtime = false;
+$liveSum = 0;
 while($getLive = mysqli_fetch_assoc($dbLive)) {
-    $liveData[] = $getLive;
     if(in_array($getLive["CountryISO"], $blacklist)) {
+        $liveData[$getLive["CountryISO"]] = 2 * $getLive["Count"]; // double impact of blacklisted countries
+    } else {
+        $liveData[$getLive["CountryISO"]] = $getLive["Count"];
+    }
+    $liveSum = $liveSum + $getLive["Count"];
+}
+
+if($liveSum > 1) { // must have adequate sample size to run analysis
+    $dbMeta = $mysqli->query("SELECT * FROM `meta_stats`");
+    $metaData = [];
+    $metaSum = 0;
+    while($getMeta = mysqli_fetch_assoc($dbMeta)) {
+        $metaData[$getMeta["CountryISO"]] = $getMeta["Count"];
+        $metaSum = $metaSum + $getMeta["Count"];
+    }
+
+    $varSum = 0;
+    $unAckLive = 0; // $ackLive found later
+    $ackMeta = 0; // $unAckMeta found later
+    $ackCount = 0;
+
+    foreach($liveData as $liveCode => $liveCount) {
+        $livePercent = $liveCount / ($liveSum + 0.000000001); // buffer against NaN if meta_stats not populated
+        if(!array_key_exists($liveCode, $metaData)) { // no known metadata
+            // echo "No key for " . $liveCode . PHP_EOL; // debug
+            $unAckLive = $unAckLive + $liveCount;
+        } else {
+            $ackCount++;
+            $metaPercent = $metaData[$liveCode] / ($metaSum + 0.000000001); // buffer against NaN if meta_stats not populated
+            $ackMeta = $ackMeta + $metaData[$liveCode];
+            // echo $liveCode . ": " . $liveCount . "/" . $liveSum . " (" . $livePercent . "), "; // debug
+            // echo $metaData[$liveCode] . "/" . $metaSum . " (" . $metaPercent . "), "; // debug
+            $thisPow = pow(($livePercent - $metaPercent), 2);
+            $varSum = $varSum + $thisPow;
+            // echo $thisPow . PHP_EOL; // debug
+        }
+    }
+
+    $ackLive = $liveSum - $unAckLive;
+    $unAckMeta = $metaSum - $ackMeta;
+    $unAckLivePercent = $unAckLive / ($liveSum + 0.000000001);
+    $unAckMetaPercent = $unAckMeta / ($metaSum + 0.000000001); // buffer against NaN if meta_stats not populated
+
+    // echo "UnAck: (L, " . $unAckLivePercent . "), (M, " . $unAckMetaPercent . ") -> "; // debug
+    $thisPow = pow(($unAckLivePercent - $unAckMetaPercent), 2);
+    $varSum = $varSum + $thisPow;
+    // echo $thisPow . PHP_EOL; // debug
+
+    $variance = $varSum / ($ackCount + 0.000000001); // didn't bother to add unacks
+    $stdDev = pow($variance, 0.5);
+    // echo "Var = " . $variance . ", StdDev = " . $stdDev . PHP_EOL; // debug
+
+    if($variance > 0.02) {
         $showtime = true;
     }
 }
 
-if($showtime) { // blacklisted, push live stats to meta
-    foreach($liveData as $toSave) {
+if($showtime) { // triggering run condition, push live stats to meta, truncate
+    foreach($liveData as $saveKey => $saveCount) {
         $mysqli->query("INSERT INTO `meta_stats` (`CountryISO`, `Count`)
-                        VALUES ('" . $toSave["CountryISO"] . "', " . $toSave["Count"] . ")
-                        ON DUPLICATE KEY UPDATE `Count` = `Count` + " . $toSave["Count"] . "");
+                        VALUES ('" . $saveKey . "', " . $saveCount . ")
+                        ON DUPLICATE KEY UPDATE `Count` = `Count` + " . $saveCount . "");
         $mysqli->query("TRUNCATE `live_stats`");
     }
-}
-
-if($showtime) {
     echo "RUN";
 } else {
     echo "WAIT";
 }
+
 $mysqli->close();
 ?>
--- server/web/index.php
+++ server/web/index.php
@@ -7,6 +7,83 @@ if($mysqli->connect_error) {
     exit;
 }
 
-echo "Zealot is running.";
+echo "<h1>Zealot</h1>";
+
+$dbBlacklist = $mysqli->query("SELECT * FROM `blacklist`");
+$blacklist = [];
+echo "<p>Blacklist: ";
+while($getBlacklist = mysqli_fetch_assoc($dbBlacklist)) {
+    $blacklist[] = $getBlacklist["CountryISO"];
+    echo $getBlacklist["CountryISO"] . " ";
+}
+echo "</p>";
+
+$dbLive = $mysqli->query("SELECT * FROM `live_stats`");
+$liveData = [];
+$liveSum = 0;
+while($getLive = mysqli_fetch_assoc($dbLive)) {
+    if(in_array($getLive["CountryISO"], $blacklist)) {
+        $liveData[$getLive["CountryISO"]] = 2 * $getLive["Count"]; // double impact of blacklisted countries
+    } else {
+        $liveData[$getLive["CountryISO"]] = $getLive["Count"];
+    }
+    $liveSum = $liveSum + $getLive["Count"];
+}
+
+echo "<h3>Country Statistics</h3>";
+if($liveSum > 1000) { // must have adequate sample size to run analysis
+    $dbMeta = $mysqli->query("SELECT * FROM `meta_stats`");
+    $metaData = [];
+    $metaSum = 0;
+    while($getMeta = mysqli_fetch_assoc($dbMeta)) {
+        $metaData[$getMeta["CountryISO"]] = $getMeta["Count"];
+        $metaSum = $metaSum + $getMeta["Count"];
+    }
+
+    $varSum = 0;
+    $unAckLive = 0; // $ackLive found later
+    $ackMeta = 0; // $unAckMeta found later
+    $ackCount = 0;
+
+    foreach($liveData as $liveCode => $liveCount) {
+        $livePercent = $liveCount / ($liveSum + 0.000000001); // buffer against NaN if meta_stats not populated
+        echo "<p>" . $liveCode . ": (live " . $liveCount . "/" . $liveSum . "), "; // debug
+        if(!array_key_exists($liveCode, $metaData)) { // no known metadata
+            $unAckLive = $unAckLive + $liveCount;
+            echo "(meta not available; country unseen)</p>";
+        } else {
+            $ackCount++;
+            $metaPercent = $metaData[$liveCode] / ($metaSum + 0.000000001); // buffer against NaN if meta_stats not populated
+            $ackMeta = $ackMeta + $metaData[$liveCode];
+            echo "(meta " . $metaData[$liveCode] . "/" . $metaSum . ") -> ";
+            $thisPow = pow(($livePercent - $metaPercent), 2);
+            $varSum = $varSum + $thisPow;
+            echo number_format(round($thisPow, 6), 6) . "</p>";
+        }
+    }
+
+    echo "<h3>Data Statistics</h3>";
+    $ackLive = $liveSum - $unAckLive;
+    $unAckMeta = $metaSum - $ackMeta;
+    $unAckLivePercent = $unAckLive / ($liveSum + 0.000000001);
+    $unAckMetaPercent = $unAckMeta / ($metaSum + 0.000000001); // buffer against NaN if meta_stats not populated
+
+    echo "Unseen %: (live " . number_format(round($unAckLivePercent, 4), 4) . "), ";
+    echo "(meta " . number_format(round($unAckMetaPercent, 4), 4) . ") -> ";
+    $thisPow = pow(($unAckLivePercent - $unAckMetaPercent), 2);
+    $varSum = $varSum + $thisPow;
+    echo number_format(round($thisPow, 4), 4) . "</p>";
+
+    $variance = $varSum / ($ackCount + 0.000000001); // didn't bother to add unacks
+    $stdDev = pow($variance, 0.5);
+    echo "<h3>Final Values</h3>";
+
+    if($variance > 0.02) {
+        echo "<p><b>Var = " . $variance . ", StdDev = " . $stdDev . "</b></p>";
+    } else {
+        echo "<p>Var = " . $variance . ", StdDev = " . $stdDev . "</p>";
+    }
+}
+
 $mysqli->close();
 ?>
